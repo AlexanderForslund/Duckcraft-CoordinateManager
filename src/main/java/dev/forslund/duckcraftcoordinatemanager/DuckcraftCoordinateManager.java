842cc6d09e37c60301614f13eeaa08dc6ab70359
Alexander Forslund <alexforslund.dev@gmail.com>
2021-12-29 15:10:47 +0100

    Initial commit

---
+++ src/main/java/dev/forslund/duckcraftcoordinatemanager/DuckcraftCoordinateManager.java
@@ -0,0 +1,17 @@
+package dev.forslund.duckcraftcoordinatemanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DuckcraftCoordinateManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
